cc190ab1b5c60aa07ece8dac9ab352d87048d270
mukesh1996-ds <mks.mukesh1996@gmail.com>
2024-03-06 13:40:29 +0530

    setup updated

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ def get_requirements_list()->List[str]:
     with open(REQUIREMENTS_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
         requirement_list = [requirement_name.replace("\n","") for requirement_name in requirement_list]
+        
         if HYPHEN_E_DOT in requirement_list:
             requirement_list.remove(HYPHEN_E_DOT)
         return requirement_list
